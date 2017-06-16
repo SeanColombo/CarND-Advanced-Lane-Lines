479d7b04084710a8efa39012cf6bbf4afacb71f9
Sean Colombo <sean.colombo@gmail.com>
2017-06-15 22:04:16 -0400

    Pulled in a lot of code from various spots to basically serve as placeholders for the rest of the pipeline and for visualizing the outputs at each step. Now there is a pipeline which outputs every step for the static images (step 3's sub-steps only store the last instance though... should probably fix that).  The curvature finding is awful (seems to be only finding the same curve for all images) and I will probably completely change the sliding-window code to use the convolutions, but there is now a pipeline that draws lanes on static images!

--- findLanes.py
+++ findLanes.py
@@ -192,8 +192,8 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap, min_y, m
     draw_lines(line_img, lines, min_y, max_y, img)
     return line_img
 
-# Python 3 has support for cool math symbols.
-
+# Python 3 has support for cool math symbols. I don't advise using these :P this function was
+# provided in an Udacity lesson though.
 def weighted_img(img, initial_img, a=0.8, ß=1., λ=0.):
     """
     `img` is the output of the hough_lines(), An image with lines drawn on it.
@@ -317,6 +317,8 @@ def process_image(image, mtx, dist, do_output=False, image_name=""):
                 and output-filenames related to this image.
     """
     
+    image_name, image_extension = os.path.splitext(image_name)
+    
     # == Use color transforms, gradients, etc., to create a thresholded binary image. ==
     color_binary = color_gradient_pipeline(image)
     if do_output == True:
@@ -336,9 +338,11 @@ def process_image(image, mtx, dist, do_output=False, image_name=""):
     triangleHeight = imgHeight * 0.44 # guess/test/revised to tune this number
     # How wide the top of the trapezoid will be (got this by tweaking until "straight lines" images transformed to be parallel.
     # This was definitely guess/test/revise and may not be appropriate for all cameras.
+    # TODO: CONTINUE TO TWEAK THIS VALUE!!! 0.35 IS VERY CLOSE, BUT OVERLAY A GRID ON IT TO SEE IF IT SHOULD BE TWEAKED MORE!!!
+    # HIGHER VALUES MAKE THEM GO OUT AT THE TOP, LOWER MAKE THEM COME BACK IN.
     trapazoidTopWidth = imgWidth * 0.35
-    xOffset = (trapazoidTopWidth / 2) # dist that trapazoid top points will be from vertical center-line
-    # Trig to figure out the points in the trapazoid based on the configuration & image size:
+    xOffset = (trapazoidTopWidth / 2) # dist that trapezoid top points will be from vertical center-line
+    # Trig to figure out the points in the trapezoid based on the configuration & image size:
     theta = math.atan( triangleHeight / (imgWidth/2) )
     topLeftX = ( (imgWidth/2) - xOffset )
     topLeftY = imgHeight - (topLeftX * math.tan(theta))
@@ -359,19 +363,195 @@ def process_image(image, mtx, dist, do_output=False, image_name=""):
                       [PADDING, imgHeight-PADDING]]) # bottom left
     # Use cv2.getPerspectiveTransform() to get M, the transform matrix
     M = cv2.getPerspectiveTransform(src, dst)
-    #Minv = cv2.getPerspectiveTransform(dst, src)
+    Minv = cv2.getPerspectiveTransform(dst, src)
     # Use cv2.warpPerspective() to warp your image to a top-down view
-    warped = cv2.warpPerspective(color_binary, M, (imgWidth, imgHeight), flags=cv2.INTER_LINEAR)
-    write_binary_image(os.path.join(OUT_DIR, "4_warped_"+image_name+".png"), warped)
-
+    binary_warped = cv2.warpPerspective(color_binary, M, (imgWidth, imgHeight), flags=cv2.INTER_LINEAR)
+    write_binary_image(os.path.join(OUT_DIR, "4_warped_"+image_name+".png"), binary_warped)
+    image_height = binary_warped.shape[0]
+    image_width = binary_warped.shape[1]
     
-    
-
-
-    # REMAINING PROJECT STEPS:
+    # == FIND LINES ==
     # Detect lane pixels and fit to find the lane boundary.
+    # === Histogram ===
+    # STARTING POINT WAS DIRECTLY FROM LESSON 14-33. Will modify from there.
+    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    plt.plot(histogram)
+    plt.savefig(os.path.join(OUT_DIR, "5_histogram_"+image_name+".png"))
+
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    
+    
+    # === Vizualizing the lane-finding ===
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, image_width)
+    plt.ylim(image_height, 0)
+    plt.savefig(os.path.join(OUT_DIR, "6_line_detection"+image_name+".png"))
+    plt.close()
+    
+    
+    
+    # TODO: MAKE SURE TO SKIP THE SLIDING-WINDOWS STEP ONCE WE HAVE AN IDEA OF WHERE THE LINES ARE (FOR PERFORMANCE REASONS).
+    # Some useful code & visualization is in the bottom of 14-33
+    
+    # NOTE: IF THE SLIDING-WINDOWS DON'T FIT WELL ENOUGH, LESSON 14-34 HAS ANOTHER WAY TO DO IT WHICH MIGHT BE EASIER
+    # TO TWEAK INTO SOMETHING THAT WOULD FIT OUR NEEDS.
+    
+    
+    
+    # == DETERMINE CURVATURE ==
     # Determine the curvature of the lane and vehicle position with respect to center.
+    # Generate some fake data to represent lane-line pixels
+    ploty = np.linspace(0, (image_height-1), num=image_height)# to cover same y-range as image
+    quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
+    # For each y position generate random x position within +/-50 pix
+    # of the line base position in each case (x=200 for left, and x=900 for right)
+    leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
+                                  for y in ploty])
+    rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
+                                    for y in ploty])
+
+    leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+    rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+
+    # Fit a second order polynomial to pixel positions in each fake lane line
+    left_fit = np.polyfit(ploty, leftx, 2)
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fit = np.polyfit(ploty, rightx, 2)
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    # Plot up the fake data
+    mark_size = 3
+    plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
+    plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
+    plt.xlim(0, image_width)
+    plt.ylim(0, image_height)
+    plt.plot(left_fitx, ploty, color='green', linewidth=3)
+    plt.plot(right_fitx, ploty, color='green', linewidth=3)
+    plt.gca().invert_yaxis() # to visualize as we do the images
+    plt.savefig(os.path.join(OUT_DIR, "7_curvature"+image_name+".png"))
+    plt.close()
+    
+    # Define y-value where we want radius of curvature
+    # I'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
+    left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+    print("\t\tCurve radii: ", left_curverad, right_curverad)
+    # Example values: 1926.74 1908.48
+    
+    
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    print("\t\tCurve radii in meters: ", left_curverad, 'm', right_curverad, 'm')
+    # Example values: 632.1 m    626.2 m
+   
+
+    # == WARP DETECTED LANE BOUNDARIES BACK ONTO THE ORIGINAL IMAGE ==
     # Warp the detected lane boundaries back onto the original image.
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
+    plt.imshow(result)
+    plt.savefig(os.path.join(OUT_DIR, "8_lane_lines_"+image_name+".png"))
+    plt.close()
+    
+    
+    # REMAINING PROJECT STEPS:
     # Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
 
 
